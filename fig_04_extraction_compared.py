b60e045b0d3a09744abbc17265a507745751d735
cylammarco <cylammarco@gmail.com>
2021-10-10 00:40:31 +0300

    Changed the fig_04 star from Hiltner102 to LHS6328 to show the difference in uncertainties in the faint ends.

--- fig_04_extraction_compared.py
+++ fig_04_extraction_compared.py
@@ -5,7 +5,7 @@ from matplotlib.gridspec import GridSpec
 import copy
 import numpy as np
 
-data = fits.open('v_s_20180810_27_1_0_2.fits.gz')[0].data
+data = fits.open('v_e_20180810_12_1_0_2.fits.gz')[0].data
 
 twodspec = spectral_reduction.TwoDSpec(data,
                                        cosmicray=True,
@@ -17,6 +17,7 @@ twodspec.ap_trace(nspec=1, nwindow=10, display=False)
 # Tophat
 twodspec.ap_extract(apwidth=10, skywidth=5, skysep=0, skydeg=1, optimal=False)
 count_tophat = copy.deepcopy(twodspec.spectrum_list[0].count)
+count_tophat_err = copy.deepcopy(twodspec.spectrum_list[0].count_err)
 img_residual_tophat = copy.deepcopy(twodspec.img_residual)
 
 # Horne86
@@ -27,6 +28,7 @@ twodspec.ap_extract(apwidth=10,
                     optimal=True,
                     algorithm='horne86')
 count_horne86 = copy.deepcopy(twodspec.spectrum_list[0].count)
+count_horne86_err = copy.deepcopy(twodspec.spectrum_list[0].count_err)
 img_residual_horne86 = copy.deepcopy(twodspec.img_residual)
 
 # Marsh89
@@ -40,6 +42,7 @@ twodspec.ap_extract(apwidth=10,
                     nreject=0,
                     qmode='fast-linear')
 count_marsh89 = copy.deepcopy(twodspec.spectrum_list[0].count)
+count_marsh89_err = copy.deepcopy(twodspec.spectrum_list[0].count_err)
 img_residual_marsh89 = copy.deepcopy(twodspec.img_residual)
 
 fig = plt.figure(1, figsize=(6, 6))
@@ -51,33 +54,36 @@ ax1 = fig.add_subplot(gs[0])
 ax2 = fig.add_subplot(gs[1:3])
 ax3 = fig.add_subplot(gs[3:])
 
-ax1.imshow(data, origin='lower', aspect='auto')
+data_logged = np.log10(data)
+data_logged[np.isnan(data_logged)] = np.nanmin(data_logged)
+
+ax1.imshow(data_logged, origin='lower', aspect='auto')
 ax1.plot(np.array(twodspec.spectrum_list[0].trace) - 0.5,
          lw=1,
          ls=':',
          color='black')
 ax1.set_xticks([])
-ax1.set_ylim(120, 145)
+ax1.set_ylim(115, 140)
 ax1.set_ylabel('Pixel (Spatial)')
 
-ax2.plot(count_tophat, color='red', label='Tophat', lw=3)
-ax2.plot(count_horne86, color='green', label='Horne86', lw=2, ls='dashed')
+ax2.plot(count_tophat, color='red', label='Tophat', lw=1)
+ax2.plot(count_horne86, color='green', label='Horne86', lw=1, ls='dashed')
 ax2.plot(count_marsh89, color='blue', label='Marsh89', lw=1, ls=':')
 
+
+ax2.set_xticks([])
 ax2.set_ylabel(r'e$^{-}$ count')
-ax2.set_ylim(0, 45000)
+ax2.set_ylim(0, 2200)
 ax2.legend(framealpha=0.9)
 
-ax3.plot(np.array(count_horne86) / np.array(count_tophat),
-         color='red',
-         label='Horne86 / Tophat')
-ax3.plot(np.array(count_horne86) / np.array(count_marsh89),
-         color='blue',
-         label='Horne86 / Marsh89')
+ax3.plot(count_tophat_err, color='red', label='Tophat', lw=1)
+ax3.plot(count_horne86_err, color='green', label='Horne86', lw=1, ls='dashed')
+ax3.plot(count_marsh89_err, color='blue', label='Marsh89', lw=1, ls=':')
+
 ax3.legend(loc='upper left', framealpha=0.9)
-ax3.set_ylim(0.995, 1.055)
+#ax3.set_ylim(0.995, 1.055)
 ax3.set_xlabel('Pixel (Dispersion)')
-ax3.set_ylabel(r'e$^-$ count Ratio (Spatial)')
+ax3.set_ylabel(r'e$^-$ count uncertainty')
 
 ax1.set_xlim(0, 1024)
 ax2.set_xlim(0, 1024)
